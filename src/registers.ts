caeb0709d9d075ab170c691994e33bc50c9725cb
19EB <systemance@gmail.com>
2026-04-04 11:05:43 +0300

    pass websocket client to event registers

--- src/registers.ts
+++ src/registers.ts
@@ -4,8 +4,8 @@ import { PreviousSongAction } from "./ws-events/previous-song";
 import { WebsocketMessage } from "./ws-events/types";
 import { registerSongChangeListener } from "./ws-listeners/song-change"
 
-export const registerListeners = () => {
-    registerSongChangeListener();
+export const registerListeners = (websocketClient: WebsocketClient) => {
+    registerSongChangeListener(websocketClient);
 }
 
 const eventHandlers = [
@@ -13,14 +13,18 @@ const eventHandlers = [
     PreviousSongAction,
 ]
 
-export const registerEvents = () => {
-    const ws = WebsocketClient.getWebsocket();
+export const registerEvents = (websocketClient: WebsocketClient) => {
+    const ws = websocketClient.getWebsocket();
+    if (!ws) {
+        console.error("Websocket is not initialized");
+        return;
+    }
 
     ws.onmessage = function (message) {
         const { data } = message;
-        const parsed:WebsocketMessage<any> = JSON.parse(data);
+        const parsed: WebsocketMessage<any> = JSON.parse(data);
         const foundAction = eventHandlers.find(action => action.eventName === parsed.eventName);
-        if(foundAction) {
+        if (foundAction) {
             foundAction.execute(parsed.payload);
         } else {
             console.log(`No handler found for event type: ${parsed.eventName}`);
--- src/ws-listeners/song-change.ts
+++ src/ws-listeners/song-change.ts
@@ -9,22 +9,22 @@ export type SongChangePayload = {
 
 const WS_EVENT_NAME = "UpdateSong";
 
-const handleSongChange = () => {
-    const currentTrack = Spicetify.Player.data.item.name;
-    const artistList = Spicetify.Player.data.item.artists ?? [];
-    const currentArtist = artistList.map(artist => artist.name).join(', ');
-    const payload: SongChangePayload = {
-        title: `${currentTrack} by ${currentArtist}`,
-        artist: currentArtist,
-        song: currentTrack,
-    };
-    const objectToSend: WebsocketEvent<SongChangePayload> = {
-      eventName: WS_EVENT_NAME,
-      payload: payload,
-    };
-    WebsocketClient.sendWebsocketMessage(objectToSend);
+const handleSongChange = (websocketClient: WebsocketClient) => {
+  const currentTrack = Spicetify.Player.data.item.name;
+  const artistList = Spicetify.Player.data.item.artists ?? [];
+  const currentArtist = artistList.map(artist => artist.name).join(', ');
+  const payload: SongChangePayload = {
+    title: `${currentTrack} by ${currentArtist}`,
+    artist: currentArtist,
+    song: currentTrack,
+  };
+  const objectToSend: WebsocketEvent<SongChangePayload> = {
+    eventName: WS_EVENT_NAME,
+    payload: payload,
+  };
+  websocketClient.sendWebsocketMessage(objectToSend);
 }
 
-export const registerSongChangeListener = () => {
-    Spicetify.Player.addEventListener("songchange", handleSongChange);
+export const registerSongChangeListener = (websocketClient: WebsocketClient) => {
+  Spicetify.Player.addEventListener("songchange", () => handleSongChange(websocketClient));
 }
