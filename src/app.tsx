a443e622588d0fd51d9f488e347bb023fb602da7
19EB <systemance@gmail.com>
2026-04-04 11:06:46 +0300

    add status button to profile menu + misc stuff

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,58 @@
-import { registerEvents, registerListeners } from "./registers";
+import { addSettings, defaultWebsocketAddress, defaultWebsocketPort, settings } from "./config/settings";
+import { GetWebsocketStatusIcon, WebsocketConnectionStatus } from "./svg/WebsocketSvg";
 import { WebsocketClient } from "./ws-api";
+import PubSub from "pubsub-js";
+
+const getStatusText = (status: WebsocketConnectionStatus): string => {
+  switch (status) {
+    case WebsocketConnectionStatus.CONNECTED:
+      return "Websocket connected";
+    case WebsocketConnectionStatus.DISCONNECTED:
+      return "Websocket disconnected";
+    case WebsocketConnectionStatus.CONNECTING:
+      return "Websocket connecting";
+    case WebsocketConnectionStatus.NOT_CONNECTED:
+      return "Websocket not connected";
+    default:
+      return "Websocket status unknown";
+  }
+};
 
 async function main() {
-
   // Wait for Spicetify to be ready
   while (!Spicetify?.showNotification) {
     await new Promise(resolve => setTimeout(resolve, 100));
   }
 
-  const client = WebsocketClient.getWebsocket();
+  let status: WebsocketConnectionStatus = WebsocketConnectionStatus.NOT_CONNECTED;
 
-  client.onerror = function () {
-    console.log('Connection Error');
-    Spicetify.showNotification("Websocket connection error");
-  };
+  const btn = new Spicetify.ContextMenuV2.Item({
+    children: getStatusText(status),
+    leadingIcon: null,
+    trailingIcon: GetWebsocketStatusIcon({ status }),
+    onClick: () => {
+      if (status === WebsocketConnectionStatus.DISCONNECTED || status === WebsocketConnectionStatus.NOT_CONNECTED) {
+        reconnect();
+      }
+    },
+    divider: false,
+  });
 
-  client.onopen = function () {
-    if (client.readyState === client.OPEN) {
-      Spicetify.showNotification("Websocket connection established");
-      // Register event listeners for outgoing events
-      registerListeners();
-      // Set up event handlers for incoming messages
-      registerEvents();
-    }
-  };
-  
-  client.onclose = function () {
-    console.log('WebSocket Client Closed');
-  };
+  btn.register();
+
+  PubSub.subscribe("websocket:status-change", () => {
+    status = websocketClient.getStatus();
+    btn.trailingIcon = GetWebsocketStatusIcon({ status });
+    btn.children = getStatusText(status);
+  });
+
+  await addSettings();
+
+  const websocketClient = new WebsocketClient();
+
+  const reconnect = () => {
+    websocketClient.reconnect();
+  }
 }
 
 export default main;
