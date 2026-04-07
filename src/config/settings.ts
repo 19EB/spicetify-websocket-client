59dcd1dfa666a66c7c621199ba48d87c64ddebf6
19EB <systemance@gmail.com>
2026-04-07 15:11:37 +0300

    remove websocket prefix from settings

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -8,9 +8,9 @@ export const defaultWebsocketAddress = "127.0.0.1";
 export const defaultWebsocketPort = "9090";
 
 export async function addSettings() {
-    settings.addInput("websocketAddress", "Websocket address", defaultWebsocketAddress);
-    settings.addInput("websocketPort", "Websocket port", defaultWebsocketPort, undefined, 'number');
-    settings.addInput("websocketEndpoint", "Websocket endpoint", "/");
-    settings.addToggle("startWebsocketOnLaunch", "Start websocket on launch", false);
+    settings.addInput("websocketAddress", "Address", defaultWebsocketAddress);
+    settings.addInput("websocketPort", "Port", defaultWebsocketPort, undefined, 'number');
+    settings.addInput("websocketEndpoint", "Endpoint", "/");
+    settings.addToggle("startWebsocketOnLaunch", "Start on launch", false);
     await settings.pushSettings();
 }
