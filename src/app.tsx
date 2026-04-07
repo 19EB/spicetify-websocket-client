ba0c1f9df77311b2ff5a4bdfb0c36a2e2bd24743
19EB <systemance@gmail.com>
2026-04-07 15:10:55 +0300

    simplify init loop

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,9 @@
-import { addSettings, defaultWebsocketAddress, defaultWebsocketPort, settings } from "./config/settings";
-import { GetWebsocketStatusIcon, WebsocketConnectionStatus } from "./svg/WebsocketSvg";
+import { WebsocketButton } from "./components/WebsocketButton";
+import { addSettings } from "./config/settings";
 import { WebsocketClient } from "./ws-api";
-import PubSub from "pubsub-js";
 
-const getStatusText = (status: WebsocketConnectionStatus): string => {
-  switch (status) {
-    case WebsocketConnectionStatus.CONNECTED:
-      return "Websocket connected";
-    case WebsocketConnectionStatus.DISCONNECTED:
-      return "Websocket disconnected";
-    case WebsocketConnectionStatus.CONNECTING:
-      return "Websocket connecting";
-    case WebsocketConnectionStatus.NOT_CONNECTED:
-      return "Websocket not connected";
-    default:
-      return "Websocket status unknown";
-  }
-};
+
+const { React, ReactDOM } = Spicetify;
 
 async function main() {
   // Wait for Spicetify to be ready
@@ -24,34 +11,21 @@ async function main() {
     await new Promise(resolve => setTimeout(resolve, 100));
   }
 
-  let status: WebsocketConnectionStatus = WebsocketConnectionStatus.NOT_CONNECTED;
-
-  const btn = new Spicetify.ContextMenuV2.Item({
-    children: getStatusText(status),
-    leadingIcon: null,
-    trailingIcon: GetWebsocketStatusIcon({ status }),
-    onClick: () => {
-      if (status === WebsocketConnectionStatus.DISCONNECTED || status === WebsocketConnectionStatus.NOT_CONNECTED) {
-        reconnect();
-      }
-    },
-    divider: false,
-  });
-
-  btn.register();
-
-  PubSub.subscribe("websocket:status-change", () => {
-    status = websocketClient.getStatus();
-    btn.trailingIcon = GetWebsocketStatusIcon({ status });
-    btn.children = getStatusText(status);
-  });
-
   await addSettings();
 
   const websocketClient = new WebsocketClient();
+  globalThis.websocketClient = websocketClient;
 
-  const reconnect = () => {
-    websocketClient.reconnect();
+  const extraControls = document.querySelector(".main-nowPlayingBar-extraControls");
+  while (!extraControls) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+
+  if (extraControls) {
+    const newContainer = document.createElement("div");
+    newContainer.style.pointerEvents = "all";
+    extraControls.insertBefore(newContainer, extraControls.firstElementChild);
+    ReactDOM.render(<WebsocketButton />, newContainer);
   }
 }
 
