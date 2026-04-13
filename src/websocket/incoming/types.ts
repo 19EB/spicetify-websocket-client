c2e2dd205e38ed9d5729887eb08eded1e7a29089
19EB <systemance@gmail.com>
2026-04-13 19:44:37 +0300

    update types to no longer require generics for better typing

--- src/websocket/incoming/types.ts
+++ src/websocket/incoming/types.ts
@@ -4,18 +4,32 @@ export enum WEBSOCKET_EVENT_TYPES {
     SET_VOLUME = "SetVolume",
 }
 
+type CommonWebsocketMessage = {
+    requestId?: string; // Optional requestId for correlating requests and responses
+}
+
 type WebsocketMessageMap = {
     [WEBSOCKET_EVENT_TYPES.NEXT_SONG]: {}; // No payload expected for this event
     [WEBSOCKET_EVENT_TYPES.PREVIOUS_SONG]: {}; // No payload expected for this event
     [WEBSOCKET_EVENT_TYPES.SET_VOLUME]: { volume: number };
 }
 
-export type WebsocketAction<K extends WEBSOCKET_EVENT_TYPES> = {
-    eventName: K;
-    execute: (payload: WebsocketMessageMap[K]) => void;
-}
-
-export type WebsocketMessage<K extends WEBSOCKET_EVENT_TYPES> = {
+export type WebsocketMessageGuard<K extends WEBSOCKET_EVENT_TYPES> = {
     eventName: K;
     payload: WebsocketMessageMap[K];
-}
+} & CommonWebsocketMessage;
+
+export type WebsocketAction = {
+    [K in WEBSOCKET_EVENT_TYPES]: {
+        eventName: K;
+        execute: (message: WebsocketMessageGuard<K>) => void;
+    }
+}[WEBSOCKET_EVENT_TYPES];
+
+export type WebsocketMessage = {
+    [K in WEBSOCKET_EVENT_TYPES]: {
+        requestId?: string; // Optional requestId for correlating requests and responses
+        eventName: K;
+        payload: WebsocketMessageMap[K];
+    } & CommonWebsocketMessage;
+}[WEBSOCKET_EVENT_TYPES];
