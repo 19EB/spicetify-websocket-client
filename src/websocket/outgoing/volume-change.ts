c7741ccd7bf807a6f62864299a37e3f3c8bc7b58
19EB <systemance@gmail.com>
2026-06-27 18:20:50 +0300

    add volume change as an outgoing event

---
+++ src/websocket/outgoing/volume-change.ts
@@ -0,0 +1,23 @@
+import { WebsocketClient } from "../client";
+import { observeValue } from "../../observers/observe-value";
+import { WEBSOCKET_OUTGOING_EVENT_TYPE, WebsocketEvent } from "./types";
+
+type VolumeChangedPayload = {
+  level: number;
+};
+
+const sendVolume = (websocketClient: WebsocketClient, level: number) => {
+  const message: WebsocketEvent<VolumeChangedPayload> = {
+    eventName: WEBSOCKET_OUTGOING_EVENT_TYPE.VOLUME_CHANGED,
+    payload: { level },
+  };
+  websocketClient.sendWebsocketMessage(message);
+};
+
+export const registerVolumeChangeListener = (websocketClient: WebsocketClient) => {
+  observeValue<number>({
+    read: () => Spicetify.Player.getVolume(),
+    emitInitial: true,
+    onChange: (level) => sendVolume(websocketClient, level),
+  });
+};
