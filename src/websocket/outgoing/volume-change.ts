f8d73a61eaa87009c199b87ce91b983170ded846
DarockObama <therockobama75@gmail.com>
2026-07-31 22:14:59 +0200

    Disabled initial VolumeChange emit, this is already sent through InitialState

--- src/websocket/outgoing/volume-change.ts
+++ src/websocket/outgoing/volume-change.ts
@@ -17,7 +17,7 @@ const sendVolume = (websocketClient: WebsocketClient, level: number) => {
 export const registerVolumeChangeListener = (websocketClient: WebsocketClient) => {
   observeValue<number>({
     read: () => Spicetify.Player.getVolume(),
-    emitInitial: true,
+    emitInitial: false,
     debounceMs: 600,
     onChange: (level) => sendVolume(websocketClient, level),
   });
